208ae55774370772fcf540739cc9d5d676b1d1a2
dlaurianti <dlaurianti@uiowa.edu>
2024-02-08 12:25:43 -0600

    telescope ignore

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -5,8 +5,23 @@ telescope.setup{
         find_files = {
             hidden = true
         }
+    },
+    defaults = {file_ignore_patterns =
+        {
+            ".git/",
+            ".cache",
+            "%.o",
+            "%.a",
+            "%.out",
+            "%.class",
+            "%.pdf",
+            "%.mkv",
+            "%.mp4",
+            "%.zip"
+        },
     }
 }
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()

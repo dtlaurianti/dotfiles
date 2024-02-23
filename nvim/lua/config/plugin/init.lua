9bcb1397b13c62266533e5613de651987286f316
dlaurianti <dlaurianti@uiowa.edu>
2024-02-22 20:45:53 -0600

    reorged folders, lazy (i think) working

---
+++ nvim/lua/config/plugin/init.lua
@@ -0,0 +1,8 @@
+require("config.plugin.autocmd")
+require("config.plugin.colors")
+require("config.plugin.fugitive")
+require("config.plugin.harpoon")
+require("config.plugin.lsp")
+require("config.plugin.telescope")
+require("config.plugin.treesitter")
+require("config.plugin.undotree")
---
+++ nvim/lua/config/plugin/telescope.lua
@@ -0,0 +1,30 @@
+local telescope = require('telescope')
+local builtin = require('telescope.builtin')
+telescope.setup{
+    pickers = {
+        find_files = {
+            hidden = true
+        }
+    },
+    defaults = {file_ignore_patterns =
+        {
+            ".git/",
+            ".cache",
+            "%.o",
+            "%.a",
+            "%.out",
+            "%.class",
+            "%.pdf",
+            "%.mkv",
+            "%.mp4",
+            "%.zip"
+        },
+    }
+}
+
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
+end)
+return {}

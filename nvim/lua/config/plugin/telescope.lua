ff16af6a1895e6066f3a262cd2f27ccefde4f67c
dlaurianti <dlaurianti@uiowa.edu>
2024-02-22 21:31:05 -0600

    telescope keymaps

--- nvim/lua/config/plugin/telescope.lua
+++ nvim/lua/config/plugin/telescope.lua
@@ -17,13 +17,16 @@ telescope.setup{
             "%.pdf",
             "%.mkv",
             "%.mp4",
-            "%.zip"
+            "%.zip",
+            "node_modules/",
         },
     }
 }
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pgc', builtin.git_commits, {})
+vim.keymap.set('n', '<leader>pgs', builtin.git_status, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)

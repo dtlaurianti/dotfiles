33e579d18f93c4ce32f1fde51837be0de094eecd
Dylan Laurianti <dtlaurianti@gmail.com>
2024-03-08 22:19:54 -0600

    init: home-manager

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,16 +1,43 @@
 {
     description = "nixOS flakes entrypoint";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixConfig = {
+        substituters = [
+            "https://cache.nixos.org"
+                "https://nix-community.cachix.org"
+        ];
+        trusted-public-keys = [
+            "nix-community.cachix.org-1:mD9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
     };
 
-    outputs = { self, nixpkgs, ... }@inputs: {
-        nixosConfigurations.dtl = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-                ./configuration.nix
-            ];
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+        home-manager = {
+            url = "github:nix-community/home-manager/release-23.11";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+        nixosConfigurations = {
+            dtl = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                modules = [
+                    ./hardware-configuration.nix
+                    ./configuration.nix
+                    {
+                        nix.settings.trusted-users = [ "dtl" ];
+                    }
+                    home-manager.nixosModules.home-manager
+                    {
+                        home-manager.useGlobalPkgs = true;
+                        home-manager.useUserPackages = true;
+                        home-manager.users.dtl = import ./home.nix;
+                    }
+                ];
+            };
         };
     };
 }
---
+++ nixos/home.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+{
+    home.username = "dtl";
+    home.homeDirectory = "/home/dtl";
+
+    home.packages = with pkgs; [
+        # fill with user-specific packages
+    ];
+
+    programs.git = {
+        enable = true;
+        userName = "Dylan Laurianti";
+        userEmail = "dtlaurianti@gmail.com";
+    };
+
+    programs.bash = {
+        enable = true;
+        enableCompletion = true;
+    };
+    
+    home.stateVersion = "23.11";
+
+    programs.home-manager.enable = true;
+}

0cafa2c3874e7d9d0ef1aee1a5948747dd3a15b0
Dylan Laurianti <dtlaurianti@gmail.com>
2024-03-08 23:31:46 -0600

    feat: setup.sh cleans up before making symlinks

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
+rm ~/.bashrc 
 ln -s /home/$SUDO_USER/config/.bashrc ~/.bashrc 
+rm ~/.bash_profile
 ln -s  /home/$SUDO_USER/config/.bash_profile ~/.bash_profile
+rm ~/.bash_aliases
 ln -s  /home/$SUDO_USER/config/.bash_aliases ~/.bash_aliases
+rm /etc/nixos/configuration.nix
 ln -s /home/$SUDO_USER/config/nixos/configuration.nix /etc/nixos/configuration.nix
+rm /etc/nixos/hardware-configuration.nix
 ln -s /home/$SUDO_USER/config/nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
+rm /etc/nixos/flake.nix
 ln -s /home/$SUDO_USER/config/nixos/flake.nix /etc/nixos/flake.nix

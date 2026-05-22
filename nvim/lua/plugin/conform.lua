3c66ff7426df0311c0c3c54901e7cc5919d47743
Dylan Laurianti <dtlaurianti@gmail.com>
2026-05-22 01:34:58 -0400

    feat: add conform for vhdl autoformat

---
+++ nvim/lua/plugin/conform.lua
@@ -0,0 +1,15 @@
+return {
+  'stevearc/conform.nvim',
+  opts = {},
+    config = function()
+      require("conform").setup({
+          formatters_by_ft = {
+            vhdl = {"vsg"},
+          },
+          format_on_save = {
+            timeout_ms = 500,
+            lsp_format = "fallback",
+          },
+      })
+    end
+}
